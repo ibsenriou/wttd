b2f26aa017bb726886b4afb63f835ee270edfff4
ibsenriou <ibsenriou@hotmail.com>
2022-03-14 16:12:42 -0300

    Import Project

---
+++ eventex/core/apps.py
@@ -0,0 +1,10 @@
+from django.apps import AppConfig
+
+
+# class CoreConfig(AppConfig):
+#     default_auto_field = 'django.db.models.BigAutoField'
+#     name = 'core'
+
+class CoreConfig(AppConfig):
+    name = 'eventex.core'
+    verbose_name = 'Core'
